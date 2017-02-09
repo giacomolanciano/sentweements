5233a79aacd4d563bf6992f50c1bb5d1bd9539e5
FanTempTime <f4b.r0s@gmail.com>
2017-02-09 20:37:32 +0100

    Working API client, implementing a sample call.

--- emotions.py
+++ emotions.py
@@ -1,20 +1,67 @@
-import http.client, urllib.request, urllib.parse, urllib.error, base64
+import requests
 
-headers = {
-    # Request headers
-    'Content-Type': 'application/json',
-    'Ocp-Apim-Subscription-Key': '{subscription key}',
-}
+# Variables
 
-params = urllib.parse.urlencode({
-})
+_url = 'https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize'
+_key = '98e72b2c085545e9b53f93d9210faa55'
+_maxNumRetries = 10
 
-try:
-    conn = http.client.HTTPSConnection('westus.api.cognitive.microsoft.com')
-    conn.request("POST", "/emotion/v1.0/recognize?%s" % params, "{body}", headers)
-    response = conn.getresponse()
-    data = response.read()
-    print(data)
-    conn.close()
-except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+def processRequest(json, headers, data=None, params=None):
+    """
+    Helper function to process the request to Microsoft's APIs.
+
+    Parameters:
+    json: Used when processing images from its URL.
+    headers: Used to pass the key information and the data type request.
+    data: Used when processing image read from disk.
+    params: Query string parameters (not used).
+    """
+
+    retries = 0
+    result = None
+
+    while True:
+
+        response = requests.request('post', _url, json=json, data=data, headers=headers, params=params)
+
+        if response.status_code == 429:
+            print("Message: %s" % (response.json()['error']['message']))
+
+            if retries <= _maxNumRetries:
+                time.sleep(1)
+                retries += 1
+                continue
+            else:
+                print('Error: failed after retrying!')
+                break
+
+        elif response.status_code == 200 or response.status_code == 201:
+            if 'content-length' in response.headers and int(response.headers['content-length']) == 0:
+                result = None
+            elif 'content-type' in response.headers and isinstance(response.headers['content-type'], str):
+                if 'application/json' in response.headers['content-type'].lower():
+                    result = response.json() if response.content else None
+                elif 'image' in response.headers['content-type'].lower():
+                    result = response.content
+        else:
+            print("Error code: %d" % (response.status_code))
+            print("Message: %s" % (response.json()['error']['message']))
+
+        break
+
+    return result
+
+
+# URL direction to image
+urlImage = 'https://raw.githubusercontent.com/Microsoft/ProjectOxford-ClientSDK/master/Face/Windows/Data/detection3.jpg'
+
+headers = dict()
+headers['Ocp-Apim-Subscription-Key'] = _key
+headers['Content-Type'] = 'application/json'
+
+json = { 'url': urlImage }
+
+result = processRequest( json, headers )
+
+print(result)
