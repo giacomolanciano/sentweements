99ccd22f50969d62b33bcb97b230e284e966105c
Giacomo <g.lanciano1@gmail.com>
2017-02-13 16:44:33 +0100

    add cursoring logic in search api call

--- tweets_rest.py
+++ tweets_rest.py
@@ -2,7 +2,7 @@ import datetime
 import json
 
 import geocoder
-from tweepy import OAuthHandler
+from tweepy import OAuthHandler, Cursor
 from tweepy.api import API
 
 import emotions
@@ -13,7 +13,7 @@ from secret_keys import TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET
 
 auth = OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
 auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
-search_api = API(auth)
+api_client = API(auth)
 
 # headers for Microsoft Emotion API request
 headers = dict()
@@ -42,51 +42,57 @@ class ImageRetriever(object):
         self.sentiments_mean = [0] * emotions.SENTIMENTS_NUM
 
     def search_api_request(self):
-        # TODO handle tweets block logic
+
+        geocode = None
 
         if self.since_date:
-            # TODO extract date and add operator to query
+            # TODO extract date and add since operator to query
             since = datetime.strptime()
         if self.until_date:
-            # TODO extract date and add operator to query
+            # TODO extract date and add until operator to query
             until = datetime.strptime()
+        if self.location_params:
+            geocode = ','.join(self.location_params)
 
-        result = search_api.search(q=query, lang=language, geocode=','.join(self.location_params))
+        cursor = Cursor(api_client.search, q=query, lang=language, geocode=geocode)
+        for page in cursor.pages():
+            for tweet in page:
+                print(tweet)
+                # self._process_tweet(tweet)
 
-    def _process_result(self, json_response):
-        """ Process a block of tweets (update mean sentiments vector). """
+    def _process_tweet(self, status):
+        """ Update mean sentiments vector upon seeing new image in a tweet. """
 
-        tweets_block = json.loads(json_response)  # refer to https://dev.twitter.com/overview/api/tweets for tweet fields
+        tweet = json.load(status._json)
 
-        for tweet in tweets_block:
-            try:
-                media_contents = tweet['entities']['media']  # list of images related to tweet
-                image_sentiments = None
-                image_url = ''
+        try:
+            media_contents = tweet['entities']['media']  # list of images related to tweet
+            image_sentiments = None
+            image_url = ''
 
-                for image in media_contents:
-                    image_url = image['media_url']
+            for image in media_contents:
+                image_url = image['media_url']
 
-                    image_sentiments = emotions.process_request(image_url, headers)
+                image_sentiments = emotions.process_request(image_url, headers)
 
-                    # process info about each face in image
-                    for face_analysis in image_sentiments:
-                        # update sentiments mean
-                        self.sample_size += 1
-                        scores = list(face_analysis['scores'].values())
-                        statistics.online_vectors_mean(self.sentiments_mean, scores, self.sample_size)
+                # process info about each face in image
+                for face_analysis in image_sentiments:
+                    # update sentiments mean
+                    self.sample_size += 1
+                    scores = list(face_analysis['scores'].values())
+                    statistics.online_vectors_mean(self.sentiments_mean, scores, self.sample_size)
 
-                        if self.print_progress:
-                            print(self.sentiments_mean)
+                    if self.print_progress:
+                        print(self.sentiments_mean)
 
-                if self.print_progress and image_sentiments:
-                    print(image_url)
-                    print(image_sentiments)
-                    print('')
+            if self.print_progress and image_sentiments:
+                print(image_url)
+                print(image_sentiments)
+                print('')
 
-            except KeyError as e:
-                # print('Missing %s key in json.' % str(e), '\n')
-                pass
+        except KeyError as e:
+            # print('Missing %s key in json.' % str(e), '\n')
+            pass
 
 
 if __name__ == '__main__':
@@ -99,6 +105,9 @@ if __name__ == '__main__':
     # to be concatenated with space when passed to api call
     location_params = [str(location.lat), str(location.lng), '1mi']
 
-    # make Search API call
-    result = search_api.search(q=query, lang=language)
-    print(result)
+    # # make Search API call
+    # result = search_api.search(q=query, lang=language)
+    # print(result)
+
+    ir = ImageRetriever(query)
+    ir.search_api_request()
