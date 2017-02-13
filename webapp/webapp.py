c72aadd5164650e70e77c6ebe3a6bc3623070abd
FanTempTime <f4b.r0s@gmail.com>
2017-02-13 16:34:58 +0100

    Configures multiprocessing in webapp.

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, send_file, request
 from flask_socketio import SocketIO, emit
+import multiprocessing
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+subprocesses = {}
+
 
 @app.route('/')
 def index():
@@ -13,24 +16,34 @@ def index():
     until_date = request.args.get('until_date')
     language = request.args.get('language')
     location = request.args.get('location')
-    if query:
-        start_time = time.time()
+    if len(request.args):  # we have at least a search argument
+
+        # TODO: define target function
+        t = multiprocessing.Process(name=request.sid, target=insert_target_function_here)
+        subprocesses[request.sid] = t
+        t.start()
 
         ir = ImageRetriever(query, since_date, until_date, language, location)
-        # TODO start request
+        # TODO: start request
 
-        elapsed = time.time() - start_time
+        # return the basic result page skeleton
         # return render_template('query_results.html', query=query, time=elapsed, results=results)
     else:
-        return send_file('index.html')
+        return send_file('index.html')  # return the homepage
+
 
 # @socketio.on('connect')
 # def on_connect():
 #     pass
 
+
 @socketio.on('disconnect')
 def test_disconnect():
     print('Client disconnected')
+    # on disconnect we destroy the corresponding thread
+    t = subprocesses.pop(request.sid)
+    t.terminate()
+
 
 @socketio.on('my event')
 def test_message(message):
