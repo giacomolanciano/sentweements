8aeaa173d65b9a365c8b59d14969669a6fb96ff0
FanTempTime <f4b.r0s@gmail.com>
2017-02-13 15:55:44 +0100

    Basic Flask-SocketIO configuration.

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -1,14 +1,13 @@
-import time
-
-from flask import Flask, send_file, request
-
-from tweets_rest import ImageRetriever
+from flask import Flask, render_template, send_file, request
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 @app.route('/')
-def root():
+def index():
+    """Serve the client-side application."""
     query = request.args.get('query')
     since_date = request.args.get('since_date')
     until_date = request.args.get('until_date')
@@ -25,6 +24,17 @@ def root():
     else:
         return send_file('index.html')
 
+# @socketio.on('connect')
+# def on_connect():
+#     pass
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
+
+@socketio.on('my event')
+def test_message(message):
+    emit('my response', {'data': 'got it!'})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')  # set debug to True to allow auto-reloading during development
+    socketio.run(app)
