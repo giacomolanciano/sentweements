2aa40f0c82e705ed6c73834204125d92e35aa19d
Giacomo Lanciano <g.lanciano1@gmail.com>
2017-02-22 20:03:43 +0100

    update streaming error message

--- tweets_streaming.py
+++ tweets_streaming.py
@@ -170,7 +170,7 @@ class RegionListener(StreamListener):
 
     def on_error(self, status):
         if self.debug:
-            print(status)
+            print(self.region_name + ': ' + status)
         return True
 
 
